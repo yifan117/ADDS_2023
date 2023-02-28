8eb081d72954f4777c01572c75b7404b298ba97d
yifan117 <yifan.qiu17@gmail.com>
2023-02-28 18:27:33 +1030

    debug

--- Referee.cpp
+++ Referee.cpp
@@ -8,7 +8,7 @@ Referee::Referee(){}
 
 Player* Referee::refGame(Player* player1, Player* player2) {
 
-    // cout << "player 1: " << player1->name << " " << player1->move << " " << "player 2: " << player2->name << " " << player2->move << endl;
+    cout << "player 1: " << player1->name << " " << player1->move << " " << "player 2: " << player2->name << " " << player2->move << endl;
     if (player1->move == player2->move) cout << "same" << endl; return nullptr;
 
     if (player1->move == 'R') {
