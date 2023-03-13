3953b6a696196c14ee052a54703f311e7d610314
yifan117 <yifan.qiu17@gmail.com>
2023-03-13 14:40:50 +1030

    comp

--- Referee.cpp
+++ Referee.cpp
@@ -18,26 +18,22 @@ Referee::Referee(){
 Player* Referee::refGame(Player* player1, Player* player2) {
     Move* p1move = player1->makeMove();
     Move* p2move = player2->makeMove();
-    std::vector<std::string> movesBeaten;
-
 
     if (p1move->name == p2move->name) {
         return nullptr;
     }
 
     if (map.find(p1move->name) != map.end()) {
-        movesBeaten = map[p1move->name];
 
-        if (std::find(movesBeaten.begin(), movesBeaten.end(), p2move->name) != movesBeaten.end()) {
-            return player1;
+        for (std::string option : map[p1move->name]) {
+            if (option == p2move->name) return player1;
         }
     }
 
     if (map.find(p2move->name) != map.end()) {
-        movesBeaten = map[p2move->name];
 
-        if (std::find(movesBeaten.begin(), movesBeaten.end(), p1move->name) != movesBeaten.end()) {
-            return player2;
+        for (std::string option : map[p2move->name]) {
+            if (option == p1move->name) return player2;
         }
     }
 
