44856e30a9803930d4af49f41ddb438ed33ff0f1
yifan117 <yifan.qiu17@gmail.com>
2023-04-02 22:08:32 +0930

    comp

--- Finder.cpp
+++ Finder.cpp
@@ -4,15 +4,16 @@ using namespace std;
 
 vector<int> Finder::findSubstrings(string s1, string s2) {
     vector<int> result;
+    size_t found = 0; // holds index of last found (instead of starting at the start for every iteration, starts at last found)
 
     for (size_t i = 1; i <= s2.size(); i++) {
-        size_t found = s1.find(s2.substr(0, i));
+        found = s1.find(s2.substr(0, i), found); // pass in last found to begin search
 
         if (found != string::npos) {
             result.push_back(found);
         } else {
             result.push_back(-1);
-            break;
+            break; // if the substring is not found, breaks the loop so it doesnt waste time iterating over the whole string
         }
     }
 
