1501002c7735430c22a2f8f0a320a2659c0cf574
yifan117 <yifan.qiu17@gmail.com>
2023-05-21 15:39:28 +0930

    initial

--- Autocomplete.cpp
+++
@@ -1,9 +0,0 @@
-#include "Autocomplete.h"
-
-std::vector<std::string> Autocomplete::getSuggestions(std::string partialWord) {
-    return trie.search(partialWord);
-}
-
-void Autocomplete::insert(std::string word) {
-    trie.insert(word);
-}
